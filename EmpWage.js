e73929ad7d7562b5b6c23580c256ed7172974e98
adityak-bit <adityajsr1998@gmail.com>
2020-11-10 08:33:35 +0530

    [AK] Added : Check Employee is Absent or Present

---
+++ EmpWage.js
@@ -0,0 +1,10 @@
+//UC1 IF ELSE CONDITION
+{
+    const IS_ABSENT = 0
+    let empCheck = Math.floor(Math.random() * 10) % 2
+    if(empCheck == 0){
+    console.log("UC1- Employee is ABSENT")
+    } else{
+    console.log("UC1- Employee is PRESENT")
+    }
+}
